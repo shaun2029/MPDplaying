5427c68ea9c7b95d485c3ae4dea04f4e0e6672fa
Shaun2029 <shauns2029@gmail.com>
2017-10-22 19:12:18 +0200

    Added music mood settings.

--- main.pas
+++ main.pas
@@ -6,7 +6,8 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
-  ExtCtrls, Buttons, Menus, IniPropStorage, Unix, Pipes, Process, Settings;
+  ExtCtrls, Buttons, Menus, IniPropStorage, ComCtrls, Unix, Pipes, Process,
+  Settings, IniFiles;
 
 type
 
@@ -15,18 +16,29 @@ type
   TfrmMain = class(TForm)
     btnPrev: TBitBtn;
     btnNext: TBitBtn;
+    GroupBox1: TGroupBox;
     mnuSettings: TMenuItem;
     mMenu: TMainMenu;
     mmPlaying: TMemo;
+    pbLevelHard: TProgressBar;
+    pbLevelSoft: TProgressBar;
+    rgpHard: TRadioGroup;
+    rgpSoft: TRadioGroup;
     tmrPlaying: TTimer;
     procedure btnNextClick(Sender: TObject);
     procedure btnPrevClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure FormShow(Sender: TObject);
+    procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
     procedure mnuSettingsClick(Sender: TObject);
     procedure tmrPlayingTimer(Sender: TObject);
+    procedure FormActivate(Sender: TObject);
+    procedure pbLevelSoftContextPopup(Sender: TObject; MousePos: TPoint;
+      var Handled: Boolean);
+    procedure rgpSoftSelectionChanged(Sender: TObject);
   private
     function GetMPDHostParams: string;
+    procedure SaveSettings;
     procedure Update;
     { private declarations }
   public
@@ -124,6 +136,113 @@ begin
 }
     end;
   end;
+end;
+
+procedure TfrmMain.FormActivate(Sender: TObject);
+var
+   Cfg: TIniFile;
+   Hardness, Softness: integer;
+begin
+  try
+    Cfg := TIniFile.Create(GetUserDir + '.music-skip.conf');
+    Softness := Cfg.ReadInteger('Settings', 'Softness', 0);
+    Hardness := Cfg.ReadInteger('Settings', 'Hardness', 4);
+
+    Cfg.Free;
+  except
+    on E: Exception do
+    begin
+      ShowMessage('Exception: ' + E.Message);
+    end;
+  end;
+
+  case Softness of
+       0: rgpSoft.ItemIndex := 0;
+       1: rgpSoft.ItemIndex := 1;
+       2: rgpSoft.ItemIndex := 2;
+       3: rgpSoft.ItemIndex := 3;
+       else rgpSoft.ItemIndex := 4;
+  end;
+
+  case Hardness of
+       0: rgpHard.ItemIndex := 0;
+       1: rgpHard.ItemIndex := 1;
+       2: rgpHard.ItemIndex := 2;
+       3: rgpHard.ItemIndex := 3;
+       else rgpHard.ItemIndex := 4;
+  end;
+end;
+
+procedure TfrmMain.SaveSettings;
+var
+  F : TextFile;
+   Soft, Hard: integer;
+begin
+
+  case rgpSoft.ItemIndex of
+       0: Soft := 0;
+       1: Soft := 4;
+       2: Soft := 8;
+       3: Soft := 10;
+       else Soft := 12;
+  end;
+
+  case rgpHard.ItemIndex of
+       0: Hard := 8;
+       1: Hard := 11;
+       2: Hard := 13;
+       3: Hard := 15;
+       else Hard := 20;
+  end;
+
+  if Hard < Soft + 2 then
+     Hard := Soft + 2;
+
+  if Hard >= 16 then
+     Hard := 20;
+
+  pbLevelSoft.Position := Soft;
+  pbLevelHard.Position := Hard;
+
+  // Ensure that all music can be played.
+  if Soft <= 0 then
+     Soft := -20;
+
+  AssignFile(f,GetUserDir + '.music-skip');
+  Rewrite(f);
+  WriteLn(f, FloatToStr(Soft));
+  WriteLn(f, FloatToStr(Hard));
+  CloseFile(f);
+end;
+
+procedure TfrmMain.FormClose(Sender: TObject; var CloseAction: TCloseAction);
+var
+   Cfg: TIniFile;
+begin
+  try
+    Cfg := TIniFile.Create(GetUserDir + '.music-skip.conf');
+    Cfg.WriteInteger('Settings', 'Softness', rgpSoft.ItemIndex);
+    Cfg.WriteInteger('Settings', 'Hardness', rgpHard.ItemIndex);
+    Cfg.Free;
+  except
+    on E: Exception do
+    begin
+      ShowMessage('Exception: ' + E.Message);
+    end;
+  end;
+
+  SaveSettings;
+end;
+
+procedure TfrmMain.pbLevelSoftContextPopup(Sender: TObject; MousePos: TPoint;
+  var Handled: Boolean);
+begin
+
+end;
+
+procedure TfrmMain.rgpSoftSelectionChanged(Sender: TObject);
+begin
+  SaveSettings;
 end;
 
 end.
